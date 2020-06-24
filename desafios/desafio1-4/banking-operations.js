681d376ccb44899e51901202d4377b9482370cb7
Bruno Santana <BrunoSSantana@users.noreply.github.com>
2020-06-24 11:55:38 -0300

    até desafio 1-3 +
    commit addfor desafio 1-3

--- desafios/desafio1-4/banking-operations.js
+++ desafios/desafio1-4/banking-operations.js
@@ -2,8 +2,28 @@ const user = {
     name: "Mariana",
     transactions: [],
     balance: 0
-  };
+};
 
-function createTransaction() {
-    let transaction = { type: '', value: 0 }
+console.log(`${user.balance}`)
+
+let transaction = { type: 'credit', value: 50 }
+
+function createTransaction(transaction) {
+  
+  if (transaction.type === 'credit') {
+    
+    let valorSoma = transaction.value + user.balance
+    valorSoma.push(user.balance)
+
+  } else if ( transaction.type === 'debit') {
+
+    let valorSoma = transaction.value - user.balance
+    valorSoma.push(user.balance)
+
+  } else {
+    
+    console.log('ERRO NA TRANSAÇÃO!')
+  }
 }
+
+console.log(`${user.balance}`)
